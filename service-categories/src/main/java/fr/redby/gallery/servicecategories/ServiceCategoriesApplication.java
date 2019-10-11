9ccef75218ccf5a0b3382c9bc4370ea165bd869a
Ioan Bernevig <ioan.bernevig@arhs-developments.com>
2019-10-12 00:00:44 +0200

    Initial commit for the service-categories.

---
+++ service-categories/src/main/java/fr/redby/gallery/servicecategories/ServiceCategoriesApplication.java
@@ -0,0 +1,13 @@
+package fr.redby.gallery.servicecategories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCategoriesApplication.class, args);
+	}
+
+}
---
+++ service-categories/src/main/java/fr/redby/gallery/servicecategories/controller/CategoriesController.java
@@ -0,0 +1,39 @@
+package fr.redby.gallery.servicecategories.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service dedicated to the management of categories.
+ * @author Ioan Bernevig
+ * @version $Revision$
+ */
+@RestController
+@RequestMapping ("/categories")
+public class CategoriesController {
+
+    public static final String GALLERY_PATH = "GALLERY_PATH";
+
+    /**
+     * @return Service retrieving all categories (folder names) and returns them as-is.
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<String> getCategories() {
+        File directory = new File(System.getProperty(GALLERY_PATH));
+        if (directory.isDirectory()) {
+            return Arrays.stream(directory.listFiles(f -> f.isDirectory()))
+                    .map(f -> f.getName())
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+}
