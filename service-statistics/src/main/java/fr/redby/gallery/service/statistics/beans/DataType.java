4791db21235194efd933f65ac29c1bf20d1b2649
Ioan <i.bernevig@gmail.com>
2019-11-15 00:36:46 +0100

    Moved size per year outside the class.

---
+++ service-statistics/src/main/java/fr/redby/gallery/service/statistics/beans/DataType.java
@@ -0,0 +1,42 @@
+package fr.redby.gallery.service.statistics.beans;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class DataType implements Serializable, Comparable<SizePerYear.DataType> {
+    public long x;
+    public int y;
+
+    public DataType() {
+        super();
+    }
+
+    public DataType(int year, int folderSize) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.DAY_OF_YEAR, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        this.x = cal.getTime().getTime();
+        this.y = folderSize;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override public int compareTo(SizePerYear.DataType o) {
+        return new Long(x).compareTo(new Long(o.x));
+    }
+}
--- service-statistics/src/main/java/fr/redby/gallery/service/statistics/beans/SizePerYear.java
+++ service-statistics/src/main/java/fr/redby/gallery/service/statistics/beans/SizePerYear.java
@@ -9,24 +9,6 @@ import java.util.*;
 
 public class SizePerYear implements Serializable {
 
-    public static class DataType implements Serializable, Comparable<DataType> {
-        public long x;
-        public int y;
-
-        public DataType(int year, int folderSize) {
-            Calendar cal = Calendar.getInstance();
-            cal.set(Calendar.YEAR, year);
-            cal.set(Calendar.DAY_OF_YEAR, 1);
-            cal.set(Calendar.HOUR_OF_DAY, 0);
-            this.x = cal.getTime().getTime();
-            this.y = folderSize;
-        }
-
-        @Override public int compareTo(DataType o) {
-            return new Long(x).compareTo(new Long(o.x));
-        }
-    }
-
     @Id
     private Integer id;
 
@@ -40,6 +22,30 @@ public class SizePerYear implements Serializable {
         this.date = new Date();
         this.data = new ArrayList<>();
         this.id = SizePerYearRepository.STAT_IDENTIFIER;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setData(List<DataType> data) {
+        this.data = data;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Date getDate() {
