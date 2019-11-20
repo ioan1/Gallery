204be3bc67967c500290a34009096a271da3ea61
Ioan Bernevig <ioan.bernevig@arhs-developments.com>
2019-11-20 01:04:18 +0100

    Added picture viewer.

--- web-ui/src/app/components/album/album.component.ts
+++ web-ui/src/app/components/album/album.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit } from '@angular/core';
+import {ActivatedRoute } from '@angular/router';
 import {PicturesService} from "../../services/pictures.service";
 import {NgxMasonryOptions} from "ngx-masonry";
+import Viewer from "viewerjs";
 
 @Component({
   selector: 'app-album',
@@ -15,6 +16,7 @@ export class AlbumComponent implements OnInit {
   pictures  : any;
   videos    : any; // TODO
   others    : any; // TODO
+  gallery   : any;
 
   public masonryOptions: NgxMasonryOptions = {
     transitionDuration: '0.5s',
@@ -25,21 +27,39 @@ export class AlbumComponent implements OnInit {
   constructor(private route: ActivatedRoute, private picturesService: PicturesService) { }
 
   ngOnInit() {
-
     this.route.paramMap.subscribe(pageParameters => {
       this.category = pageParameters.get('idcat');
       this.album = pageParameters.get('idalbum');
       // Fetch the pictures within this category
-      this.picturesService.listPictures(this.category, this.album).subscribe((data: {}) => {
-        this.pictures = data;
-        console.log(data);
+        this.picturesService.listPictures(this.category, this.album).subscribe((data: {}) => {
+          // render thumbnails
+          this.pictures = data;
+          console.log(data);
+          setTimeout(this.startGallery, 500); // Obliged to have a small delay when starting the gallery, letting time maybe for the DOM to be there ...
       })
     });
 
   }
 
-  showPicture(category: number, album: string, picture: string) {
-    alert('show picture: ' + picture);
+  /**
+   * See: https://github.com/fengyuanchen/viewerjs/blob/master/README.md
+   * TODO: ensure this is called only when all thumbnails are loaded !!
+   */
+  startGallery() {
+    this.gallery = new Viewer(document.getElementById('pictures'), {
+      url(image) {
+        return image.src.replace('/small/', '/full/');
+      },
+    });
+    console.log('Starting the gallery');
+    this.gallery.full();
+  }
+
+  public ngOnDestroy() {
+    if (this.gallery != undefined) {
+      console.log('Destroying the gallery');
+      this.gallery.destroy();
+    }
   }
 
 }
