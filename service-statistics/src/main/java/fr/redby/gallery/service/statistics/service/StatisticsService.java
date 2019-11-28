cdbac2756d4cec09869d39509430f87d27cf4d54
Ioan Bernevig <ioan.bernevig@arhs-developments.com>
2019-11-29 00:16:12 +0100

    sonar

--- service-statistics/src/main/java/fr/redby/gallery/service/statistics/service/StatisticsService.java
+++ service-statistics/src/main/java/fr/redby/gallery/service/statistics/service/StatisticsService.java
@@ -11,9 +11,11 @@ import redis.clients.jedis.Jedis;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,23 +23,21 @@ import org.slf4j.LoggerFactory;
 /**
  * Service dedicated to any statistical processing.
  */
-@Service
-public class StatisticsService {
+@Service public class StatisticsService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger( StatisticsService.class );
+    private static final Logger LOGGER = LoggerFactory.getLogger(StatisticsService.class);
 
-    private static final long GB = 1024l*1024l*1024l;
+    private static final long GB = 1024l * 1024l * 1024l;
     private static final String WAIT_FOR_PROCESSING = "A_TRIER";
     private static final String REDIS = "redis";
 
-    @Autowired
-    private SizePerYearRepository sizePerYearRepository;
+    @Autowired private SizePerYearRepository sizePerYearRepository;
 
-    @Autowired
-    private PicturesPerYearRepository picturesPerYearRepository;
+    @Autowired private PicturesPerYearRepository picturesPerYearRepository;
 
     /**
      * Returns the disk usage (free space vs used).
+     *
      * @return DiskUsage entity
      */
     public DiskUsage getDiskUsage() {
@@ -46,7 +46,7 @@ public class StatisticsService {
         LOGGER.info("Free space in {}: {}", System.getProperty("GALLERY_PATH"), root.getFreeSpace());
         LOGGER.info("Usable space in {}: {}", System.getProperty("GALLERY_PATH"), root.getUsableSpace());
 
-        int used = (int) (root.getTotalSpace()/GB - root.getFreeSpace()/GB);
+        int used = (int) (root.getTotalSpace() / GB - root.getFreeSpace() / GB);
         int available = (int) (root.getTotalSpace() / GB);
 
         return new DiskUsage(used, available);
@@ -54,6 +54,7 @@ public class StatisticsService {
 
     /**
      * Returns the sizes of each category per year.
+     *
      * @return Everything is wrapped into a SizePerYear entity.
      * @throws IOException any error is returned to the calling method.
      */
@@ -66,14 +67,16 @@ public class StatisticsService {
             SizePerYear result = new SizePerYear();
             File root = new File(System.getProperty("GALLERY_PATH"));
             for (File folder : root.listFiles(f -> f.isDirectory())) {
-                if (!folder.getName().matches("[0-9]+")) continue;
-                int folderSize = (int) (Files.walk(folder.toPath())
-                        .filter(p -> p.toFile().isFile())
-                        .mapToLong(p -> p.toFile().length())
-                        .sum() / GB);
-                int year = Integer.parseInt(folder.getName());
-                LOGGER.info("{} => {}", year, folderSize);
-                result.getData().add(new DataType(year, folderSize));
+                if (!folder.getName().matches("[0-9]+"))
+                    continue;
+                try (Stream<Path> stream = Files.walk(folder.toPath())) {
+                    int folderSize =
+                            (int) (stream.filter(p -> p.toFile().isFile()).mapToLong(p -> p.toFile().length()).sum()
+                                    / GB);
+                    int year = Integer.parseInt(folder.getName());
+                    LOGGER.info("{} => {}", year, folderSize);
+                    result.getData().add(new DataType(year, folderSize));
+                }
             }
             Collections.sort(result.getData());
             LOGGER.info("Saving the size per year into the database.");
@@ -88,13 +91,12 @@ public class StatisticsService {
      */
     public Long getWaitForProcessing() throws IOException {
         File root = new File(System.getProperty("GALLERY_PATH") + File.separator + WAIT_FOR_PROCESSING);
-        return Files.walk(root.toPath())
-                .filter(p -> p.toFile().isFile())
-                .count();
+        return Files.walk(root.toPath()).filter(p -> p.toFile().isFile()).count();
     }
 
     /**
      * Return the pictures per year statistics wrapped within a PicturesPerYearWrapper.
+     *
      * @return PicturesPerYearWrapper
      */
     public PicturesPerYearWrapper getPicturesPerYear() {
