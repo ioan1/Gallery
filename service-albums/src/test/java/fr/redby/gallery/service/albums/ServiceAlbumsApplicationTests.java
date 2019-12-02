73882887540ff7c93068fc2f9a6b3431fdc21179
Ioan Bernevig <ioan.bernevig@arhs-developments.com>
2019-12-03 00:57:00 +0100

    test

--- service-albums/src/test/java/fr/redby/gallery/service/albums/ServiceAlbumsApplicationTests.java
+++ service-albums/src/test/java/fr/redby/gallery/service/albums/ServiceAlbumsApplicationTests.java
@@ -1,5 +1,6 @@
 package fr.redby.gallery.service.albums;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ServiceAlbumsApplicationTests {
 
-
+	@Test
+	public void contextLoads() {
+        Assert.assertNotNull(this);
+	}
 
 }
