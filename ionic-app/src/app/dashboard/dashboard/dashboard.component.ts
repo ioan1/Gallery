e98517c1ebc2b2a6568752c5d168bb2fc52d2e2b
Bernevig Ioan <>
2020-01-21 22:50:33 +0100

    Created the dashboard module.

---
+++ ionic-app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {DiskUsage, StatisticsService} from "../../services/statistics.service";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit {
+
+  private diskUsage: DiskUsage;
+  private waitForProcessing: Number;
+  private cachedThumbnails: Number;
+
+  constructor(private statisticsService: StatisticsService) { }
+
+  ngOnInit() {
+    /** Disk usage **/
+    this.statisticsService.getDiskUsage().subscribe((data: DiskUsage) => {
+      this.diskUsage = data;
+    });
+    /** Files wait for processing **/
+    this.statisticsService.getWaitForProcessing().subscribe((data: Number) => {
+      this.waitForProcessing = data;
+    });
+    /** Thumbnails that are cached via redis. **/
+    this.statisticsService.getCachedThumbnails().subscribe((data: Number) => {
+      this.cachedThumbnails = data;
+    })
+  }
+
+}
