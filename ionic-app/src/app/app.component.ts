2c68894688cee3323495c540b5e7b3b179e08a38
Bernevig Ioan <>
2020-05-05 00:12:26 +0200

    Hide menu when not authenticated.

--- ionic-app/src/app/app.component.ts
+++ ionic-app/src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
-        authenticationService: AuthenticationService,
+        public authenticationService: AuthenticationService,
     ) {
         this.initializeApp();
     }
