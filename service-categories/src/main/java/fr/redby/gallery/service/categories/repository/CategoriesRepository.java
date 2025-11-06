fd28e9e21a72226053d70427c3ef79f2f19b6f6c
Bernevig Ioan <12858327+ioan1@users.noreply.github.com>
2025-11-06 09:07:14 +0000

    Removed old java project files.

--- service-categories/src/main/java/fr/redby/gallery/service/categories/repository/CategoriesRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.redby.gallery.service.categories.repository;
-
-import fr.redby.gallery.service.categories.bean.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoriesRepository extends MongoRepository<Category, String> {
-    //
-}
--- service-pictures/src/main/java/fr/redby/gallery/servicepictures/repositories/ExifDataRepository.java
+++
@@ -1,9 +0,0 @@
-
-package fr.redby.gallery.servicepictures.repositories;
-
-import fr.redby.gallery.servicepictures.bean.ExifData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ExifDataRepository extends MongoRepository<ExifData, String> {
-    //
-}
