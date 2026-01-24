ad4317fc91b9b0ed39aaa3152edc8a41b610663b
Bernevig Ioan <12858327+ioan1@users.noreply.github.com>
2026-01-24 00:19:26 +0000

    Changing OIDC redirection url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_bHxrl8GNt",
   client_id: "4cm803bb86anli21j7nf29lvhh",
-  redirect_uri: "https://gallery.redby.fr/login",
+  redirect_uri: "https://gallery.redby.fr",
   response_type: "code",
   scope: "phone openid email",
 };
